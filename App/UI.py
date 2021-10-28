723f4888ede939bf9f11fff8583cffec9c957b9c
nopby <byronharnope@gmail.com>
2021-10-28 23:40:42 +0700

    First Commit

---
+++ App/UI.py
@@ -0,0 +1,145 @@
+from tkinter import Button, Label, filedialog
+from PIL import Image
+from PIL.ImageTk import PhotoImage
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from io import BytesIO
+from matplotlib.figure import Figure
+
+
+class UI:
+    def __init__(self, hwnd):
+        self.Hwnd = hwnd
+        self.ImageLabels = []
+        self.Images = []
+        self.OutputImage = Label(self.Hwnd)
+        self.ImageCount = 0
+        self.OpenImageBtn()
+        self.SaveImageBtn()
+        self.PrevImageBtn()
+        self.NextImageBtn()
+
+    def OpenImageBtn(self):
+        Button(self.Hwnd, text="Open Image", 
+        command=self.OpenImageFile).grid(row=0, column=0, columnspan=3, sticky="ew")
+        
+    def PrevImageBtn(self):
+        Button(self.Hwnd, text="<<", 
+        command=self.PrevImage).grid(row=1, column=0, sticky="ns")
+
+    def NextImageBtn(self):
+        Button(self.Hwnd, text=">>", 
+        command=self.NextImage).grid(row=1, column=2, sticky="ns")
+
+    def SaveImageBtn(self):
+        Button(self.Hwnd, text="Save Image", 
+        command=self.SaveImage).grid(row=2, column=0, columnspan=3, sticky="nsew")
+
+    def OpenImageFile(self):
+        filename = filedialog.askopenfilename(title="Open Image", filetypes=[("image file", (".jpg"))])
+
+        if filename:
+
+            if self.Images and self.ImageLabels:
+                self.Images.clear()
+                self.ImageLabels.clear()
+
+            image = cv2.imread(filename)
+
+            image = cv2.resize(image, dsize=(512, 512), interpolation=cv2.INTER_NEAREST)
+            
+            facemask = np.zeros(image.shape[:2]).astype(np.uint8)
+            mouthmask = np.zeros(image.shape[:2]).astype(np.uint8)
+            
+
+            faceCascade = cv2.CascadeClassifier("App/Model/Face.xml")
+            mouthCascade = cv2.CascadeClassifier("App/Model/Mouth.xml")
+
+            faces = faceCascade.detectMultiScale(image, 1.3, 5)
+            mouth = mouthCascade.detectMultiScale(image, 1.3, 5)
+
+            for ((x0, y0, w0, h0), (x1, y1, w1, h1)) in zip(faces, mouth):
+                cv2.rectangle(facemask, (x0, y0), (x0+w0, y0+h0), (255, 255, 255), -1)
+                cv2.rectangle(mouthmask, (x1, y1), (x1+w1, y1+h1), (255, 255, 255), -1)
+
+            
+
+
+            colors = ['b', 'g', 'r']
+            fPltHisto = Figure()
+            f = fPltHisto.add_subplot(221)
+            m = fPltHisto.add_subplot(222)
+            for i, color in enumerate(colors):
+                f_hist_mask = cv2.calcHist([image],[i], facemask,[256],[0,256])
+                f.plot(f_hist_mask, c=color)
+                m_hist_mask = cv2.calcHist([image],[i], mouthmask,[256],[0,256])
+                m.plot(m_hist_mask, c=color)
+            
+            f.title.set_text("Face Masked Histogram")
+            m.title.set_text("Mouth Masked Histogram")
+            faceimghisto = BytesIO()
+            fPltHisto.savefig(faceimghisto)
+
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            faceMaskedColor = cv2.bitwise_and(image, image, mask=facemask)
+            mouthImMasked = cv2.bitwise_and(image, image, mask=mouthmask)
+
+            image = Image.fromarray(image)
+            facemask = Image.fromarray(facemask)
+            mouthmask = Image.fromarray(mouthmask)
+            faceMaskedColor = Image.fromarray(faceMaskedColor)
+            mouthImMasked = Image.fromarray(mouthImMasked)
+            fhisto = Image.open(faceimghisto).convert("RGB")
+
+            self.Images.append(image)
+            
+            self.Images.append(facemask)
+            self.Images.append(mouthmask)
+
+            self.Images.append(faceMaskedColor)
+            self.Images.append(mouthImMasked)
+            self.Images.append(fhisto)
+            image = PhotoImage(image)
+
+            facemask = PhotoImage(facemask)
+            mouthmask = PhotoImage(mouthmask)
+
+            faceMaskedColor = PhotoImage(faceMaskedColor)
+            mouthImMasked = PhotoImage(mouthImMasked)
+            fhisto = PhotoImage(fhisto)
+            self.ImageLabels.append(image)
+
+            self.ImageLabels.append(facemask)
+            self.ImageLabels.append(mouthmask)
+
+            self.ImageLabels.append(faceMaskedColor)
+            self.ImageLabels.append(mouthImMasked)
+            self.ImageLabels.append(fhisto)
+
+            # Output Image with Created Image Labels
+            self.OutputImage = Label(image=self.ImageLabels[0])
+            self.OutputImage.grid(row=1, column=1)
+
+    def NextImage(self):
+        if self.ImageLabels and self.ImageCount + 1 < len(self.ImageLabels):       
+            self.ImageCount = self.ImageCount + 1
+            self.OutputImage.grid_forget()
+            self.OutputImage = Label(image=self.ImageLabels[self.ImageCount])
+            self.OutputImage.grid(row=1, column=1)
+
+    def PrevImage(self):
+        if self.ImageLabels and self.ImageCount - 1 > -1:       
+            self.ImageCount = self.ImageCount - 1
+            self.OutputImage.grid_forget()
+            self.OutputImage = Label(image=self.ImageLabels[self.ImageCount])
+            self.OutputImage.grid(row=1, column=1)
+    
+    def SaveImage(self):
+        if self.ImageLabels:
+            filedirectory = filedialog.asksaveasfile(mode="w", defaultextension=".jpg")
+            if not filedirectory:
+                return
+            self.Images[self.ImageCount].save(filedirectory)
+            
+
